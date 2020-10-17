73d7bc74c83e2bd11c8b2673771707cb6d9525ea
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-10-17 07:35:52 +0100

    Level 500 DONE

--- script.js
+++ script.js
@@ -69,69 +69,158 @@ document.getElementById("js-filtered-result").innerHTML = getAllEpisodes().lengt
 
 let selectBox = document.getElementById("exampleFormControlSelect1");
 
-fetch("https://api.tvmaze.com/shows/82/episodes")
+function fetchData(id){
+fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
 .then(response => response.json())
 .then(function (tvEpisodes){
+  selectBox.innerHTML = "";
 
-   tvEpisodes.map(tvEpisode => {
+  tvEpisodes.map(tvEpisode => {
   let session = tvEpisode.season.toString().length===1 ? '0'+ tvEpisode.season : tvEpisode.season;
 
 
   let numbers = tvEpisode.number.toString().length===1 ? '0' + tvEpisode.number : tvEpisode.number;
 
+  
   selectBox.innerHTML += `
   
   <option id="select" value="${tvEpisode.name}">S${session}E${numbers}-${tvEpisode.name}</option>
   
   `;
+  root.innerHTML += `
+  
+  <div class="col-sm-6 col-md-4 col-lg-3 mb-4 js-series-card border movies-card"
+    <div class="card shadow-sm rounded" style="width: 18rem;">
+      <img class="card-img-top rounded-top"  src="${tvEpisode.image.medium}" alt="${tvEpisode.name} image">
+      <div class="card-body">
+        <h5 class="card-title">${tvEpisode.name}</h5>
+        <p class="card-text">${tvEpisode.summary.substring(0,150)}...</p>
+        <a href="${tvEpisode.url}" target="_blank" class="btn btn-primary">View Details</a>
+      </div>
+    </div>
+  </div>
+
+`
 })
 })
 .catch(error=>console.log(error))
-
+}
 selectBox.addEventListener('change', (event) => {
   searchTitles(event.target.value);
 });
 
 //LEVEL 400
 
+
+
+
 let showAll = getAllShows()
 
 let showBox = document.getElementById("showTime");
 
+//showBox.innerHTML= "";
+
 showAll.map(tvShows => {
-showBox.innerHTML += `
+
+  showBox.innerHTML += `
   
   <option id="select" value="${tvShows.id}">${tvShows.name}</option>
   
   `;
 })
+
+
+
 function myFunction (){
 
-  root.innerHTML = "";
-  showAll.map(tvEpisode => {
+ root.innerHTML = "";
 
-  let session = tvEpisode.season.toString().length===1 ? '0'+ tvEpisode.season : tvEpisode.season;
+const id = showBox.options[showBox.selectedIndex].value;
 
-  let numbers = tvEpisode.number.toString().length===1 ? '0' + tvEpisode.number : tvEpisode.number;
+fetchData(id);
 
-  root.innerHTML += `
+  //showAll.map(tvEpisode => {
   
-    <div class="col-sm-6 col-md-4 col-lg-3 mb-4 js-series-card border movies-card"
-      <div class="card shadow-sm rounded" style="width: 18rem;">
-        <img class="card-img-top rounded-top"  src="${tvEpisode.image.medium}" alt="${tvEpisode.name} image">
-        <div class="card-body">
-          <h5 class="card-title">${tvEpisode.name}-S${session}E${numbers}</h5>
-          <p class="card-text">${tvEpisode.summary.substring(0,150)}...</p>
-          <a href="${tvEpisode.url}" target="_blank" class="btn btn-primary">View Details</a>
-        </div>
-      </div>
-    </div>
-  
-  `;
-})
+  // let session = tvEpisode.season.toString().length===1 ? '0'+ tvEpisode.season : tvEpisode.season;
+
+  // let numbers = tvEpisode.number.toString().length===1 ? '0' + tvEpisode.number : tvEpisode.number;
+
+//})
 }
 
 
+
+
+function addButtonToShowsPage() {
+  const inputs = document.querySelector('.inputs');
+  const buttonToShowsView = addElement('button', inputs, 'shows-page-btn');
+  buttonToShowsView.textContent = 'Browse Shows';
+
+  buttonToShowsView.addEventListener('click', homepageHandler);
+}
+
+function clearPage() {
+  const headerInputs = document.querySelector('.inputs');
+  main.textContent = '';
+  headerInputs.textContent = '';
+}
+
+function makeShowsPage(shows = allShows, searchTerm) {
+  let sortedShows;
+  if (sortByRating) {
+    // sort by rating high to low
+    sortedShows = shows.sort((a, b) => b.rating.average - a.rating.average);
+  } else {
+    // sort by name
+    sortedShows = shows.sort((a, b) => a.name.localeCompare(b.name));
+  }
+  makeShowSelector(sortedShows);
+  makeShowSortSelector();
+  makeShowSearchBar(searchTerm);
+  makePageForShows(sortedShows);
+}
+
+function makeEpisodesPage(id) {
+  makeEpisodesSearchBar();
+
+  const loadInfo = makeLoader();
+  const title = getShowTitleFromId(id);
+
+  loadInfo.textContent = `Loading episodes for ${title}...`;
+
+  fetchEpisodesForShow(id)
+    .then((data) => {
+      if (data) {
+        loadInfo.remove();
+        makeEpisodeSelector();
+        makePageForEpisodes(data);
+        addButtonToShowsPage();
+
+        const state = { episodeList: data };
+
+        history.pushState(state, 'episode');
+      } else {
+        throw new Error("Data didn't come from API");
+      }
+    })
+    .catch((err) => {
+      loadInfo.textContent =
+        'Error while fetching data, please try again. You will be redirected to shows page after 3 seconds.';
+      setTimeout(() => {
+        clearPage();
+        makeShowsPage();
+      }, 3000);
+    });
+}
+
+function makeToTopButton() {
+  const header = document.querySelector('header');
+  const toTopButton = addElement('div', header, 'top');
+  toTopButton.textContent = 'Top';
+
+  toTopButton.addEventListener('click', handleToTopButton);
+}
+//-S${session}E${numbers}
 
 // let id = showAll.map(tvShows => {
 //   fetch(`https://api.tvmaze.com/shows/${tvShows.id}/episodes`)
@@ -287,3 +376,29 @@ function myFunction (){
 //  * Adds options to car select
 //  **/
 // addOptions(carsSelect, cars);
+
+// let showAll = getAllShows()
+
+// function selectShows (allShows) {
+//   var episodeSelector = document.getElementById ('showTime');
+//   episodeSelector.value = 'default';
+//   episodeSelector.textContent = '';
+
+//   names = [];
+//   allShows.forEach (element => {
+//     names.push (element.name);
+//   });
+//   names = names.sort ();
+//   var showSelector = document.getElementById ('select-shows');
+//   for (let i = 0; i < allShows.length; i++) {
+//     var option = document.createElement ('option');
+//     text = document.createTextNode (`${names[i]} `);
+//     for (j = 0; j < allShows.length; j++) {
+//       if (names[i] === allShows[j].name) {
+//         option.setAttribute ('value', allShows[j].id);
+//       }
+//     }
+//     option.appendChild (text);
+//     showSelector.insertBefore (option, select.lastChild);
+//   }
+// }
